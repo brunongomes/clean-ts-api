7e835fb737d8b30f7098f9ffc78cf4ce02923eb9
brunongs <brunongomes8@gmail.com>
2023-05-29 13:38:24 -0300

    refactor: fix typo

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  covaregeDirectory: 'coverage',
-  testeEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
